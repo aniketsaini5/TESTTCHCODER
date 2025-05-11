989e485fa354348f23b8bca83131bc092d2ebcc8
Aniket Saini <154746704+aniketsaini5@users.noreply.github.com>
2025-05-11 23:13:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 // Start coding here
 console.log("Hello, TeamCode Hub!");
+console.log(3+34+34-34)
+console.log(3+34+34-34)
+console.log(3+34+34-34)
+console.log(3+34+34-34)
+console.log(3+34+34-34)
+console.log(3+34+34-34)
+console.log(3+34+34-34)
